18c529533daf51499e5f3484b50e6cad0520339e
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-11-13 15:27:13 +0300

    First commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+            self._value = ""
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        if not self._value:
+            return 0
+            
+        parts = re.split(r'[.?!]+', self._value)
+        
+        count = 0
+        for part in parts:
+             if part.strip():
+                 count += 1
+                 
+        count = 0
+        for part in parts:
+            if part.strip():
+                count += 1
+        
+        return count
